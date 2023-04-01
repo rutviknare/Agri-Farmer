9d6426b3b59f85c4a7cf68ef77f1399ff93098a9
rutviknare <rutviknare16@gmail.com>
2023-04-01 10:40:44 +0530

    01-04-2023 uddated

---
+++ app/src/main/java/com/example/agri_farmer/splash_main.java
@@ -0,0 +1,32 @@
+package com.example.agri_farmer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash_main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // on below line we are
+                // creating a new intent
+                Intent i = new Intent(splash_main.this, MainActivity.class);
+
+                // on below line we are
+                // starting a new activity.
+                startActivity(i);
+
+                // on the below line we are finishing
+                // our current activity.
+                finish();
+            }
+        }, 2000);
+    }
+}
